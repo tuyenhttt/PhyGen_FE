5d4fd2515ed1b98052b9a969f59b6b9d291fec07
Thanh Tuyền <tuyen13022003@gmail.com>
2025-06-07 22:16:32 +0700

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from '@/routes';
 import { ToastContainer } from 'react-toastify';
+import ScrollToTop from '@/utils/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <AppRoutes />
       <ToastContainer position='top-right' autoClose={2000} />
     </BrowserRouter>
