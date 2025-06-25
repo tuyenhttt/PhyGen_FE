92e1166a5947a6fb2ec6140e2698e7f746ca8be7
HungNTK <hungntkqe170153@fpt.edu.vn>
2025-06-25 10:26:30 +0700

    Call API for Payment

---
+++ src/services/paymentService.js
@@ -0,0 +1,46 @@
+import axiosClient from '@/services/axiosClient';
+
+const API = '/api/payments';
+
+const createPayment = async ({
+  userId,
+  amount,
+  returnUrl,
+  cancelUrl,
+}) => {
+  return await axiosClient.post(`${API}/create-payment`, {
+  userId,
+  amount,
+  returnUrl,
+  cancelUrl,
+  });
+};
+
+const getPaymentStatus = async paymentLinkId => {
+  return await axiosClient.get(`${API}/status/${paymentLinkId}`);
+};
+
+const getWebhook = async (paymentLinkId) => {
+  return await axiosClient.get(`${API}/webhook?PaymentLinkId=${paymentLinkId}`);
+};
+
+const searchPayments = async ({ userId, fromDate, toDate, status, pageIndex = 1, pageSize = 10 }) => {
+  return await axiosClient.get(`${API}/search`, {
+    params: {
+      userId,
+      FromDate: fromDate,
+      ToDate: toDate,
+      Status: status,
+      PageIndex: pageIndex,
+      PageSize: pageSize
+    }
+  });
+};
+
+
+export {
+  createPayment,
+  getPaymentStatus,
+  getWebhook,
+  searchPayments
+};
